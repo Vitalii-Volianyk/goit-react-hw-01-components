2b552ec1fea858615a33aa68ab73859ef5a91361
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2022-12-10 13:28:01 +0200

    modified:   src/components/TaskList/TaskList.jsx

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -3,16 +3,16 @@ export default function TaskList() {
   return (
     <ul className="task-list">
       <li>
-        <a href="/task1">Task 1</a>
+        <a href="/goit-react-hw-01-components#task1">Task 1</a>
       </li>
       <li>
-        <a href="/task2">Task 2</a>
+        <a href="/goit-react-hw-01-components#task2">Task 2</a>
       </li>
       <li>
-        <a href="/task3">Task 3</a>
+        <a href="/goit-react-hw-01-components#task3">Task 3</a>
       </li>
       <li>
-        <a href="/task4">Task 4</a>
+        <a href="/goit-react-hw-01-components#task4">Task 4</a>
       </li>
     </ul>
   );
