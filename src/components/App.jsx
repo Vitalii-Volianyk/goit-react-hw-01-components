4aba9c1a3fcba43059300c5a0e551815ad2c799a
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2022-12-11 21:22:49 +0200

    modified:   src/components/App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,89 +9,18 @@ import transactions from '../data/transactions.json';
 import Profile from './Profile/Profile';
 
 export function App() {
-  switch (window.location.hash) {
-    case '#task1':
-      return (
-        <>
-          <a href="/goit-react-hw-01-components#list">Go to list</a>
-          <div className="profiles">
-            <Profile
-              username={user.username}
-              tag={user.tag}
-              location={user.location}
-              avatar={user.avatar}
-              stats={user.stats}
-            />
-            <Profile
-              username={user.username}
-              tag={user.tag}
-              location={user.location}
-              avatar={user.avatar}
-              stats={user.stats}
-            />
-            <Profile
-              username={user.username}
-              tag={user.tag}
-              location={user.location}
-              avatar={user.avatar}
-              stats={user.stats}
-            />
-            <Profile
-              username={user.username}
-              tag={user.tag}
-              location={user.location}
-              avatar={user.avatar}
-              stats={user.stats}
-            />
-            <Profile
-              username={user.username}
-              tag={user.tag}
-              location={user.location}
-              avatar={user.avatar}
-              stats={user.stats}
-            />
-          </div>
-        </>
-      );
-    case '#task2':
-      return (
-        <>
-          <a href="/goit-react-hw-01-components#list">Go to list</a>
-          <Statistics title="Upload stats" stats={data} />
-          <Statistics stats={data} />
-        </>
-      );
-    case '#task3':
-      return (
-        <>
-          <a href="/goit-react-hw-01-components#list">Go to list</a>
-          <FriendList friends={friends} />
-        </>
-      );
-    case '#task4':
-      return (
-        <>
-          <a href="/goit-react-hw-01-components#list">Go to list</a>
-          <TransactionHistory data={transactions} />
-        </>
-      );
-    case '#list':
-      return <TaskList />;
-    default:
-      return (
-        <>
-          <Profile
-            username={user.username}
-            tag={user.tag}
-            location={user.location}
-            avatar={user.avatar}
-            stats={user.stats}
-          />
-          <Statistics title="Upload stats" stats={data} />
-          <Statistics stats={data} />
-          <FriendList friends={friends} />
-          <TransactionHistory data={transactions} />
-        </>
-      );
-  }
+  return (
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory data={transactions} />
+    </>
+  );
 }
