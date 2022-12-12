ba2942b030417827c08a852c58b9e77db1ebf491
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2022-12-12 11:05:12 +0200

    modified:   src/components/Statistics/Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,10 @@
 import { PropTypes } from 'prop-types';
 import './Statistics.css';
 import StatisticsItem from './StatisticItem';
-export default function Statistics({ title = 'Statistics', stats }) {
+export default function Statistics({ title, stats }) {
   return (
     <section className="statistics">
-      <h2 className="title">{title}</h2>
+      {title && <h2 className="title">{title}</h2>}
 
       <ul className="stat-list">
         {stats.map(item => (
